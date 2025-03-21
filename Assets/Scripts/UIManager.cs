b3776157a08c07671260ba64b48c8e3642827fca
高潇屹 <1583233179@qq.com>
2025-03-21 19:24:16 +0800

    UIManager

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    private static UIManager instance;
+    public static UIManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
